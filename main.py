41bcd47d5a18e5f15cae25e54d50a551ee82ff35
viruna123 <virunews2010@gmail.com>
2026-02-25 15:27:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,15 +20,17 @@ if __name__ == "__main__":
             v1, v2, audio = data['video1'], data['video2'], data['audio']
             ticker, title = data['ticker_text'], data['overlay_title']
 
-            print("Starting Stream with Original Audio Balance... 🏏🦁")
+            print("Starting Stream with Aspect Ratio Fix... 🛠️🏏")
             
-            # [0:a][1:a]...volume=3.0 -> මැච් එකේ සද්දේ ආයේ පට්ටටම වැඩි කළා
-            # [2:a]volume=0.02 -> පාපරේ සින්දුව ආයේ හීනියටම හැදුවා
+            # setsar=1:1 සහ scale පාවිච්චි කරලා වීඩියෝ දෙකම එකම මට්ටමට ගත්තා
+            # force_original_aspect_ratio=decrease: වීඩියෝ එක ඇද නොවී තියාගන්නවා
             
             cmd = (
                 f'ffmpeg -re -stream_loop -1 -i "{v1}" -stream_loop -1 -i "{v2}" -stream_loop -1 -i "{audio}" '
-                f'-filter_complex "[0:v]scale=1280:720[v1s]; [1:v]scale=1280:720[v2s]; [v1s][v2s]concat=n=2:v=1[v]; '
-                f'[v]drawtext=text=\'{title}\':x=20:y=20:fontsize=30:fontcolor=yellow:box=1:boxcolor=black@0.6, '
+                f'-filter_complex "[0:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,setsar=1,format=yuv420p[v1s]; '
+                f'[1:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,setsar=1,format=yuv420p[v2s]; '
+                f'[v1s][v2s]concat=n=2:v=1:a=0[v_base]; '
+                f'[v_base]drawtext=text=\'{title}\':x=20:y=20:fontsize=30:fontcolor=yellow:box=1:boxcolor=black@0.6, '
                 f'drawtext=text=\'{ticker}\':x=w-mod(t*100\\,w+tw):y=h-50:fontsize=25:fontcolor=white:box=1:boxcolor=black@0.7[vf]; '
                 f'[0:a][1:a]concat=n=2:v=0:a=1,volume=3.0[a_match]; '
                 f'[2:a]volume=0.02[a_bg]; '
@@ -43,4 +45,4 @@ if __name__ == "__main__":
         
         time.sleep(5)
 
-# Restart count: 9
+# Restart count: 10
