2635d631fb9bb553ff52073fd2c38aa841eb7061
viruna123 <virunews2010@gmail.com>
2026-02-25 13:05:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,23 +10,33 @@ if __name__ == "__main__":
     YOUTUBE_URL = "rtmp://a.rtmp.youtube.com/live2"
     STREAM_KEY = os.getenv("STREAM_KEY")
     
+    if not STREAM_KEY:
+        print("ERROR: STREAM_KEY is not set in GitHub Secrets!")
+        exit(1)
+
     while True:
         try:
             data = get_data()
             v1, v2, audio = data['video1'], data['video2'], data['audio']
             ticker, title = data['ticker_text'], data['overlay_title']
 
-            print("Starting Stream... 🏏")
-            # FFmpeg: වීඩියෝ 2 එකතු කරලා, අකුරු දාලා, 720p වලට හදනවා
+            print("Starting Stream with Resolution & Loop Fix... 🏏")
+            
+            # වීඩියෝ දෙකම එකම සයිස් එකකට (1280x720) හරවා පසුව එකතු කරන FFmpeg කෝඩ් එක
             cmd = (
                 f'ffmpeg -re -stream_loop -1 -i "{v1}" -stream_loop -1 -i "{v2}" -stream_loop -1 -i "{audio}" '
-                f'-filter_complex "[0:v][1:v]concat=n=2:v=1,scale=1280:720,format=yuv420p[v_base]; '
+                f'-filter_complex "[0:v]scale=1280:720,setsar=1[v1_scaled]; '
+                f'[1:v]scale=1280:720,setsar=1[v2_scaled]; '
+                f'[v1_scaled][v2_scaled]concat=n=2:v=1[v_base]; '
                 f'[v_base]drawtext=text=\'{title}\':x=20:y=20:fontsize=30:fontcolor=yellow:box=1:boxcolor=black@0.6, '
-                f'drawtext=text=\'{ticker}\':x=w-mod(t*100\,w+tw):y=h-50:fontsize=25:fontcolor=white:box=1:boxcolor=black@0.7[v_final]" '
-                f'-map "[v_final]" -map 2:a -c:v libx264 -preset ultrafast -b:v 2500k -maxrate 2500k -bufsize 5000k '
-                f'-g 60 -c:a aac -b:a 128k -f flv {YOUTUBE_URL}/{STREAM_KEY}'
+                f'drawtext=text=\'{ticker}\':x=w-mod(t*100\\,w+tw):y=h-50:fontsize=25:fontcolor=white:box=1:boxcolor=black@0.7[v_final]" '
+                f'-map "[v_final]" -map 2:a -c:v libx264 -preset ultrafast -tune zerolatency '
+                f'-b:v 2500k -maxrate 2500k -bufsize 5000k -g 60 '
+                f'-c:a aac -b:a 128k -f flv {YOUTUBE_URL}/{STREAM_KEY}'
             )
             os.system(cmd)
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Stream encountered an error: {e}")
+        
+        print("Restarting stream in 5 seconds...")
         time.sleep(5)
