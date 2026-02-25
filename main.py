3b9344fb4c7e64f2ba479aee026ba40bdd09d158
viruna123 <virunews2010@gmail.com>
2026-02-25 12:59:39 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import json
+import time
+
+def get_data():
+    with open('index.json', 'r') as f:
+        return json.load(f)
+
+if __name__ == "__main__":
+    YOUTUBE_URL = "rtmp://a.rtmp.youtube.com/live2"
+    STREAM_KEY = os.getenv("STREAM_KEY")
+    
+    while True:
+        try:
+            data = get_data()
+            v1, v2, audio = data['video1'], data['video2'], data['audio']
+            ticker, title = data['ticker_text'], data['overlay_title']
+
+            print("Starting Stream... 🏏")
+            # FFmpeg: වීඩියෝ 2 එකතු කරලා, අකුරු දාලා, 720p වලට හදනවා
+            cmd = (
+                f'ffmpeg -re -stream_loop -1 -i "{v1}" -stream_loop -1 -i "{v2}" -stream_loop -1 -i "{audio}" '
+                f'-filter_complex "[0:v][1:v]concat=n=2:v=1,scale=1280:720,format=yuv420p[v_base]; '
+                f'[v_base]drawtext=text=\'{title}\':x=20:y=20:fontsize=30:fontcolor=yellow:box=1:boxcolor=black@0.6, '
+                f'drawtext=text=\'{ticker}\':x=w-mod(t*100\,w+tw):y=h-50:fontsize=25:fontcolor=white:box=1:boxcolor=black@0.7[v_final]" '
+                f'-map "[v_final]" -map 2:a -c:v libx264 -preset ultrafast -b:v 2500k -maxrate 2500k -bufsize 5000k '
+                f'-g 60 -c:a aac -b:a 128k -f flv {YOUTUBE_URL}/{STREAM_KEY}'
+            )
+            os.system(cmd)
+        except Exception as e:
+            print(f"Error: {e}")
+        time.sleep(5)
